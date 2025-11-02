34db15db0c90d00d87b97eed0eef7513844cbffc
Lucas Paiva <lpaivamatiazzi@gmail.com>
2025-11-01 23:58:26 -0300

    Add solutions and dataset for part 4

--- part_4/Main.java
+++ part_4/Main.java
@@ -1,6 +1,82 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-	public static void main(String[] args) {
-			
-	}
+    public static final char[][] KEYPAD = {
+            { 'A', 'B', 'C', 'D' },
+            { 'E', 'F', 'G', 'H' },
+            { 'I', 'J', 'K', 'L' },
+            { 'M', 'N', 'O', 'P' }
+    };
+
+    public static List<String> readFile() {
+        Path filepath = Path.of("hyperskill-dataset-117566097.txt");
+        List<String> lines = new ArrayList<>();
+
+        // Read all file lines, including the last empty line
+        try (RandomAccessFile raf = new RandomAccessFile(filepath.toFile(), "r")) {
+            lines.addAll(Files.readAllLines(filepath));
+
+            // Check if there is an empty last line
+            if (raf.length() > 0) {
+                raf.seek(raf.length() - 1);
+                if (raf.read() == '\n' || raf.read() == '\r') {
+                    lines.add("");
+                }
+            }
+
+            return lines;
+
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    public static char getLetterFromKeypad(String directions) {
+        int y = 0, x = 0;
+
+        for (String direction : directions.split("\\,")) {
+            switch (direction) {
+                case "UP" -> {
+                    if (y > 0) {
+                        y -= 1;
+                    }
+                }
+                case "DOWN" -> {
+                    if (y < 3) {
+                        y += 1;
+                    }
+                }
+                case "RIGHT" -> {
+                    if (x < 3) {
+                        x += 1;
+                    }
+                }
+                case "LEFT" -> {
+                    if (x > 0) {
+                        x -= 1;
+                    }
+                }
+            }
+        }
+
+        return KEYPAD[y][x];
+    }
+
+    public static void main(String[] args) {
+        List<String> sequence = readFile();
+        StringBuilder sb = new StringBuilder();
+
+        for (String directions : sequence) {
+            sb.append(getLetterFromKeypad(directions));
+        }
+        
+        System.out.println(sb);
+    }
 }
--- part_4/main.c
+++ part_4/main.c
@@ -1,6 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char KEYPAD[4][4] = {{'A', 'B', 'C', 'D'},
+                           {'E', 'F', 'G', 'H'},
+                           {'I', 'J', 'K', 'L'},
+                           {'M', 'N', 'O', 'P'}};
+
+void strip_newline(char *text) {
+    for (size_t i = 0; i < strlen(text); i++) {
+        if (text[i] == '\n') {
+            text[i] = '\0';
+        }
+    }
+}
+
+char get_letter_from_keypad(char *directions) {
+    int x = 0, y = 0;
+
+    char *tok = strtok(directions, ",");
+
+    while (tok != NULL) {
+        
+        strip_newline(tok); // strip '\n' from tokens
+        
+        if (strcmp(tok, "UP") == 0) {
+            if (y > 0)
+                y -= 1;
+        } else if (strcmp(tok, "DOWN") == 0) {
+            if (y < 3)
+                y += 1;
+        } else if (strcmp(tok, "RIGHT") == 0) {
+            if (x < 3)
+                x += 1;
+        } else if (strcmp(tok, "LEFT") == 0) {
+            if (x > 0)
+                x -= 1;
+        }
+
+        tok = strtok(NULL, ",");
+    }
+
+    return KEYPAD[y][x];
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    FILE *file = fopen("hyperskill-dataset-117566097.txt", "r");
+    if (file == NULL) {
+        perror("Error opening the file");
+        return EXIT_FAILURE;
+    }
+
+    size_t buffer_size = 2 * 1024;
+    char *buffer = calloc(buffer_size, sizeof(char));
+    if (buffer == NULL) {
+        perror("Failed to allocate memory");
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+
+    int ch = 0;
+    while ((fgets(buffer, buffer_size - 1, file)) != NULL) {
+        buffer[buffer_size - 1] = '\0';
+        ch = get_letter_from_keypad(buffer);
+        if (ch == EOF) {
+            fclose(file);
+            free(buffer);
+            return EXIT_FAILURE;
+        }
+
+        printf("%c", ch);
+        memset(buffer, 0, buffer_size); // Clean buffer
+    }
+    printf("A\n"); // Last empty line from the file
+
+    fclose(file); // Close file
+
+    free(buffer); // Free allocated memory
+
     return 0;
 }
